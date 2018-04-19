721ebef5ebb6fc68559c9e379d50414803e73f6d
DIEGO-PC\DIEGOL <die.areosa@gmail.com>
2018-04-19 19:17:40 -0300

    z

---
+++ EjemploTDD/TestCalculadoraStringTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCalculadoraStringTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SumarStringBacioTest()
+        {
+            Calculadora unaCalculadora = new Calculadora();
+            Assert AreEqual(0 unaCalculadora(¨¨));
+        }
+    }
+}
